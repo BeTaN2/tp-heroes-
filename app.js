01b345f53e72c2465628f80b5ce9eb3ba8c32c61
BeTaN2 <106564340+BeTaN2@users.noreply.github.com>
2022-06-17 20:38:27 -0300

    agrego imagenes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express();
 const port = 3030;
 
+app.use(express.static("public"));
+
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
 app.get('/babbage',(req,res) => res.sendFile(path.join(__dirname,'views','babbage.html')))
